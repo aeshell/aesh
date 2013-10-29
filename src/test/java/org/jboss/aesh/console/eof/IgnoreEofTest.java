7a42bce99b4ed6051492582d12bf9e42c6c6d7a9
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-10-29 15:14:10 -0400

    Added IgnoreEof Test

---
+++ src/test/java/org/jboss/aesh/console/eof/IgnoreEofTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2013 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Eclipse Public License version 1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.jboss.aesh.console.eof;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+import org.jboss.aesh.console.BaseConsoleTest;
+import org.jboss.aesh.console.Console;
+import org.jboss.aesh.console.ConsoleCallback;
+import org.jboss.aesh.console.ConsoleOperation;
+import org.jboss.aesh.console.settings.SettingsBuilder;
+import org.jboss.aesh.edit.Mode;
+import org.jboss.aesh.terminal.Key;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:lincolnbaxter@gmail.com">Lincoln Baxter, III</a>
+ */
+public class IgnoreEofTest extends BaseConsoleTest {
+
+    @Test
+    public void ignoreeofDefaultVi() throws IOException, InterruptedException {
+        SettingsBuilder builder = new SettingsBuilder();
+        builder.enableAlias(true);
+        builder.persistAlias(false);
+        builder.mode(Mode.VI);
+
+        PipedOutputStream stdin = new PipedOutputStream();
+        Console console = getTestConsole(builder, new PipedInputStream(stdin));
+        console.setConsoleCallback(new ConsoleCallback() {
+            @Override
+            public int readConsoleOutput(ConsoleOperation output) throws IOException {
+                return 0;
+            }
+        });
+        console.setIgnoreEof(1);
+        console.start();
+
+        String BUF = "asdfasdf";
+
+        stdin.write(BUF.getBytes());
+        Thread.sleep(100);
+
+        Assert.assertEquals(BUF, console.getBuffer());
+
+        stdin.write(Key.CTRL_D.getFirstValue());
+        stdin.flush();
+        Thread.sleep(100);
+
+        Assert.assertEquals("", console.getBuffer());
+
+        stdin.write(Key.CTRL_D.getFirstValue());
+        Thread.sleep(100);
+
+        Assert.assertFalse(console.isRunning());
+    }
+
+    @Test
+    public void ignoreeofDefaultEmacs() throws IOException, InterruptedException {
+        SettingsBuilder builder = new SettingsBuilder();
+        builder.enableAlias(true);
+        builder.persistAlias(false);
+        builder.mode(Mode.EMACS);
+
+        PipedOutputStream stdin = new PipedOutputStream();
+        Console console = getTestConsole(builder, new PipedInputStream(stdin));
+        console.setConsoleCallback(new ConsoleCallback() {
+            @Override
+            public int readConsoleOutput(ConsoleOperation output) throws IOException {
+                return 0;
+            }
+        });
+        console.start();
+        console.setIgnoreEof(1);
+
+        String BUF = "a";
+
+        stdin.write(BUF.getBytes());
+        Thread.sleep(100);
+
+        Assert.assertEquals(BUF, console.getBuffer());
+
+        for (int val : Key.LEFT.getKeyValues()) {
+            stdin.write(val);
+        }
+        stdin.flush();
+        stdin.write(Key.CTRL_D.getFirstValue());
+        Thread.sleep(100);
+        Assert.assertEquals("", console.getBuffer());
+
+        stdin.write(Key.CTRL_D.getFirstValue());
+        Thread.sleep(100);
+
+        Assert.assertFalse(console.isRunning());
+    }
+
+}
