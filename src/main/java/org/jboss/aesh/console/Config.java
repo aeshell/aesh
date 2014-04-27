6fe9b4a87f9da6f5a97b618ac73ba83c5b535c09
Ståle W. Pedersen <stalep@gmail.com>
2014-04-28 01:08:18 +0200

    [AESH-230] checkstyle fix

--- src/main/java/org/jboss/aesh/console/Config.java
+++ src/main/java/org/jboss/aesh/console/Config.java
@@ -19,7 +19,6 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
