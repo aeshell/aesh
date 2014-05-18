e5fc97f7efcd282150edffbb19ffe299acf3e73d
eprogramming <00hf11@gmail.com>
2014-05-18 09:22:57 -0300

    AliasManager: diamond op, try-with-resources...

--- src/test/java/org/jboss/aesh/console/alias/AliasManagerTest.java
+++ src/test/java/org/jboss/aesh/console/alias/AliasManagerTest.java
@@ -7,9 +7,12 @@
 package org.jboss.aesh.console.alias;
 
 import org.jboss.aesh.console.Config;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -19,10 +22,20 @@ import static org.junit.Assert.assertNull;
  */
 public class AliasManagerTest {
 
+    private AliasManager manager;
+    
+    @Before
+    public void setTup() {
+        try {
+            manager = new AliasManager(new File("foo"), false, "aesh");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
     @Test
-    public void testParseAlias() throws Exception {
-        AliasManager manager = new AliasManager(new File("foo"), false, "aesh");
-
+    public void testParseAlias() {
+        
         assertNull(manager.parseAlias("alias foo2='bar -s -h'"));
         assertNull(manager.parseAlias("alias foo=bar"));
         assertNull(manager.parseAlias("alias foo3=bar --help"));
@@ -42,16 +55,21 @@ public class AliasManagerTest {
     }
 
     @Test
-    public void testUnalias() throws Exception {
-        AliasManager manager = new AliasManager(new File("foo"), false, "aesh");
-
+    public void testUnalias() {
+        
         manager.parseAlias("alias foo2='bar -s -h'");
         manager.parseAlias("alias foo=bar");
         manager.parseAlias("alias foo3=bar --help");
 
         manager.removeAlias("unalias foo3");
         assertEquals("aesh: unalias: foo3: not found"+Config.getLineSeparator(), manager.removeAlias("unalias foo3"));
-
-
     }
+    
+    @Test
+    public void testPrintAllAliases() {
+        String alias = "alias foo='bar'";
+        manager.parseAlias(alias);
+        Assert.assertEquals(alias + Config.getLineSeparator(), manager.printAllAliases());
+    }
+    
 }
