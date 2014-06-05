848b8323def304fdd717ab07515a8880f3d9c2b5
Daniel Cunha <danielsoro@gmail.com>
2014-06-05 07:51:38 -0300

    Change to import to junit

--- src/test/java/org/jboss/aesh/cl/CommandLinePopulatorTest.java
+++ src/test/java/org/jboss/aesh/cl/CommandLinePopulatorTest.java
@@ -28,8 +28,8 @@ import org.junit.rules.ExpectedException;
 import java.io.File;
 import java.util.Currency;
 
-import static junit.framework.Assert.assertNotNull;
-import static junit.framework.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
