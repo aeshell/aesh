46c262323a82852e99ffc162e58136e00da905d4
John O'Hara <johnaohara80@gmail.com>
2014-07-22 09:35:49 +0100

    Updated error message

--- src/main/java/org/jboss/aesh/cl/parser/AeshCommandLineParser.java
+++ src/main/java/org/jboss/aesh/cl/parser/AeshCommandLineParser.java
@@ -163,7 +163,7 @@ public class AeshCommandLineParser implements CommandLineParser {
                             commandLine.addOption(active);
                             active = null;
                             if(addedArgument)
-                                commandLine.setParserException(new ArgumentParserException("An argument was given to an option that do not support it."));
+                                commandLine.setParserException(new ArgumentParserException("An argument was given to an option that does not support it."));
                         }
                     }
                 }
@@ -179,7 +179,7 @@ public class AeshCommandLineParser implements CommandLineParser {
                     commandLine.addOption(active);
                     active = null;
                     if(addedArgument)
-                        commandLine.setParserException(new ArgumentParserException("An argument was given to an option that do not support it."));
+                        commandLine.setParserException(new ArgumentParserException("An argument was given to an option that does not support it."));
                 }
                 else if(active == null)
                     commandLine.setParserException(new OptionParserException("Option: "+parseLine+" is not a valid option for this command"));
@@ -246,7 +246,7 @@ public class AeshCommandLineParser implements CommandLineParser {
                                 commandLine.addOption(active);
                                 active = null;
                                 if(addedArgument)
-                                    commandLine.setParserException( new OptionParserException("An argument was given to an option that do not support it."));
+                                    commandLine.setParserException( new OptionParserException("An argument was given to an option that does not support it."));
                             }
                         }
                     }
@@ -262,7 +262,7 @@ public class AeshCommandLineParser implements CommandLineParser {
                         commandLine.addOption(active);
                         active = null;
                         if(addedArgument)
-                            commandLine.setParserException(new OptionParserException("An argument was given to an option that do not support it."));
+                            commandLine.setParserException(new OptionParserException("An argument was given to an option that does not support it."));
                     }
                     else if(active == null)
                         commandLine.setParserException(new OptionParserException("Option: "+parseLine+" is not a valid option for this command"));
@@ -292,12 +292,12 @@ public class AeshCommandLineParser implements CommandLineParser {
                     active = null;
                 }
                 if(addedArgument)
-                    commandLine.setParserException(new OptionParserException("An argument was given to an option that do not support it."));
+                    commandLine.setParserException(new OptionParserException("An argument was given to an option that does not support it."));
             }
             //if no command is "active", we add it as an argument
             else {
                 if(command.getArgument() == null) {
-                    commandLine.setParserException(new OptionParserException("An argument was given to a command that do not support it."));
+                    commandLine.setParserException(new OptionParserException("An argument was given to a command that does not support it."));
                 }
                 else {
                     commandLine.addArgumentValue(parseLine);
