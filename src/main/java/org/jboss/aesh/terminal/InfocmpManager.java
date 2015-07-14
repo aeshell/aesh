5d57bc4a8cc7913241970bd91a561572129fd7ec
Ståle W. Pedersen <stalep@gmail.com>
2015-07-14 14:40:35 +0200

    log info is good enough

--- src/main/java/org/jboss/aesh/terminal/InfocmpManager.java
+++ src/main/java/org/jboss/aesh/terminal/InfocmpManager.java
@@ -36,7 +36,7 @@ public class InfocmpManager {
     public static int[] getKeyHome() {
         int[] home = InfocmpHandler.getInstance().getAsInts("khome");
         if(home.length == 0) {
-            LOGGER.warning("Failed to get key home from infocmp, using default");
+            LOGGER.info("Failed to get key home from infocmp, using default");
             return new int[]{27,79,72}; //use default value
         }
         else
@@ -46,7 +46,7 @@ public class InfocmpManager {
     public static int[] getHome() {
         int[] home = InfocmpHandler.getInstance().getAsInts("home");
         if(home.length == 0) {
-            LOGGER.warning("Failed to get cursor home from infocmp, using default");
+            LOGGER.info("Failed to get cursor home from infocmp, using default");
             return new int[]{27,91,72}; //use default value
         }
         else
@@ -56,7 +56,7 @@ public class InfocmpManager {
     public static int[] getEnd() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("end");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get end from infocmp, using default");
+            LOGGER.info("Failed to get end from infocmp, using default");
             return new int[]{27,91,70}; //use default value
         }
         else
@@ -66,7 +66,7 @@ public class InfocmpManager {
     public static int[] getKeyEnd() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kend");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get end from infocmp, using default");
+            LOGGER.info("Failed to get end from infocmp, using default");
             return new int[]{27,79,70}; //use default value
         }
         else
@@ -76,7 +76,7 @@ public class InfocmpManager {
     public static int[] getPgUp() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kpp");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get pgup from infocmp, using default");
+            LOGGER.info("Failed to get pgup from infocmp, using default");
             return new int[]{27,91,53,126}; //use default value
         }
         else
@@ -86,7 +86,7 @@ public class InfocmpManager {
     public static int[] getPgDown() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("knp");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get pgdown from infocmp, using default");
+            LOGGER.info("Failed to get pgdown from infocmp, using default");
             return new int[]{27,91,54,126}; //use default value
         }
         else
@@ -96,7 +96,7 @@ public class InfocmpManager {
     public static int[] getLeft() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kcub1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get left from infocmp, using default");
+            LOGGER.info("Failed to get left from infocmp, using default");
             return new int[]{27,79,68}; //use default value
         }
         else
@@ -106,7 +106,7 @@ public class InfocmpManager {
     public static int[] getRight() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("cuf1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get right from infocmp, using default");
+            LOGGER.info("Failed to get right from infocmp, using default");
             return new int[]{27,79,68}; //use default value
         }
         else
@@ -116,7 +116,7 @@ public class InfocmpManager {
     public static int[] getUp() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kcuu1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get up from infocmp, using default");
+            LOGGER.info("Failed to get up from infocmp, using default");
             return new int[]{27,79,65}; //use default value
         }
         else
@@ -126,7 +126,7 @@ public class InfocmpManager {
     public static int[] getDown() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kcud1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get down from infocmp, using default");
+            LOGGER.info("Failed to get down from infocmp, using default");
             return new int[]{27,79,66}; //use default value
         }
         else
@@ -136,7 +136,7 @@ public class InfocmpManager {
     public static int[] getIns() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kich1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get insert from infocmp, using default");
+            LOGGER.info("Failed to get insert from infocmp, using default");
             return new int[]{27,91,50,126}; //use default value
         }
         else
@@ -147,7 +147,7 @@ public class InfocmpManager {
     public static int[] getDelete() {
         int[] infocmpValue = InfocmpHandler.getInstance().getAsInts("kdch1");
         if(infocmpValue.length == 0) {
-            LOGGER.warning("Failed to get delete from infocmp, using default");
+            LOGGER.info("Failed to get delete from infocmp, using default");
             return new int[]{27,91,51,126}; //use default value
         }
         else
@@ -157,7 +157,7 @@ public class InfocmpManager {
     public static String saveCursor() {
         String cursor = InfocmpHandler.getInstance().get("sc");
         if(cursor.length() == 0) {
-            LOGGER.warning("Failed to get save_cursor from infocmp, using default");
+            LOGGER.info("Failed to get save_cursor from infocmp, using default");
             return "\u001B[s";
         }
         else
@@ -167,7 +167,7 @@ public class InfocmpManager {
     public static String restoreCursor() {
         String cursor = InfocmpHandler.getInstance().get("rc");
         if(cursor.length() == 0) {
-            LOGGER.warning("Failed to get restore_cursor from infocmp, using default");
+            LOGGER.info("Failed to get restore_cursor from infocmp, using default");
             return "\u001B[u";
         }
         else
@@ -177,7 +177,7 @@ public class InfocmpManager {
     public static String clearScreen() {
         String clear = InfocmpHandler.getInstance().get("clear");
         if(clear.length() == 0) {
-            LOGGER.warning("Failed to get clear from infocmp, using default");
+            LOGGER.info("Failed to get clear from infocmp, using default");
             return "\u001B[2J";
         }
         else
@@ -187,7 +187,7 @@ public class InfocmpManager {
     public static String alternateBuffer() {
         String buffer = InfocmpHandler.getInstance().get("smcup");
         if(buffer.length() == 0) {
-            LOGGER.warning("Failed to get alternate buffer from infocmp, using default");
+            LOGGER.info("Failed to get alternate buffer from infocmp, using default");
             return "\u001B[?1049h";
         }
         else
@@ -197,7 +197,7 @@ public class InfocmpManager {
     public static String mainBuffer() {
         String buffer = InfocmpHandler.getInstance().get("rmcup");
         if(buffer.length() == 0) {
-            LOGGER.warning("Failed to get main buffer from infocmp, using default");
+            LOGGER.info("Failed to get main buffer from infocmp, using default");
             return "\u001B[?1049l";
         }
         else
@@ -207,7 +207,7 @@ public class InfocmpManager {
     public static String invertBackground() {
         String buffer = InfocmpHandler.getInstance().get("smso");
         if(buffer.length() == 0) {
-            LOGGER.warning("Failed to invert background from infocmp, using default");
+            LOGGER.info("Failed to invert background from infocmp, using default");
             return "\u001B[7m";
         }
         else
@@ -217,7 +217,7 @@ public class InfocmpManager {
     public static String normalBackground() {
         String buffer = InfocmpHandler.getInstance().get("rmso");
         if(buffer.length() == 0) {
-            LOGGER.warning("Failed to reset to normal background from infocmp, using default");
+            LOGGER.info("Failed to reset to normal background from infocmp, using default");
             return "\u001B[27m";
         }
         else
@@ -227,7 +227,7 @@ public class InfocmpManager {
     public static String enableBold() {
         String bold = InfocmpHandler.getInstance().get("bold");
         if(bold.length() == 0) {
-            LOGGER.warning("Failed to get bold from infocmp, using default");
+            LOGGER.info("Failed to get bold from infocmp, using default");
             return "\u001B[0;1m";
         }
         else
@@ -237,7 +237,7 @@ public class InfocmpManager {
     public static String enableUnderline() {
         String underline = InfocmpHandler.getInstance().get("smul");
         if(underline.length() == 0) {
-            LOGGER.warning("Failed to get underline from infocmp, using default");
+            LOGGER.info("Failed to get underline from infocmp, using default");
             return "\u001B[0;4m";
         }
         else
@@ -247,7 +247,7 @@ public class InfocmpManager {
     public static String disableUnderline() {
         String underline = InfocmpHandler.getInstance().get("rmul");
         if(underline.length() == 0) {
-            LOGGER.warning("Failed to exit underline from infocmp, using default");
+            LOGGER.info("Failed to exit underline from infocmp, using default");
             return "\u001B[0;24m";
         }
         else
@@ -257,7 +257,7 @@ public class InfocmpManager {
     public static String enableBlink() {
         String blink = InfocmpHandler.getInstance().get("blink");
         if(blink.length() == 0) {
-            LOGGER.warning("Failed to enable blink from infocmp, using default");
+            LOGGER.info("Failed to enable blink from infocmp, using default");
             return "\u001B[0;5m";
         }
         else
@@ -267,7 +267,7 @@ public class InfocmpManager {
     public static String originalColors() {
         String reset = InfocmpHandler.getInstance().get("op");
         if(reset.length() == 0) {
-            LOGGER.warning("Failed to reset from infocmp, using default");
+            LOGGER.info("Failed to reset from infocmp, using default");
             return "\u001B[0;0m";
         }
         else
