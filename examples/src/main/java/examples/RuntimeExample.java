a3a87c7507e0a86bfe656980f7cf62a98a7d85a3
Ståle Pedersen <stalep@gmail.com>
2018-04-11 16:00:51 +0200

    added a simple example that shows how to use the runtime without a shell

---
+++ examples/src/main/java/examples/RuntimeExample.java
@@ -0,0 +1,53 @@
+package examples;
+
+import org.aesh.command.AeshCommandRuntimeBuilder;
+import org.aesh.command.Command;
+import org.aesh.command.CommandDefinition;
+import org.aesh.command.CommandException;
+import org.aesh.command.CommandNotFoundException;
+import org.aesh.command.CommandResult;
+import org.aesh.command.CommandRuntime;
+import org.aesh.command.impl.registry.AeshCommandRegistryBuilder;
+import org.aesh.command.invocation.CommandInvocation;
+import org.aesh.command.option.Option;
+import org.aesh.command.parser.CommandLineParserException;
+import org.aesh.command.validator.CommandValidatorException;
+import org.aesh.command.validator.OptionValidatorException;
+
+import java.io.IOException;
+
+public class RuntimeExample {
+
+    public static void main(String[] args) throws CommandLineParserException {
+      CommandRuntime runtime = AeshCommandRuntimeBuilder
+              .builder()
+              .commandRegistry(AeshCommandRegistryBuilder.builder().command(TestConsoleCommand.class).create())
+              .build();
+
+      String input = "test --bar FOO";
+        try {
+            runtime.executeCommand(input);
+        }
+        catch (CommandNotFoundException  e) {
+            System.out.println("Command not found: "+input);
+
+        }
+        catch (OptionValidatorException | CommandException | CommandValidatorException | IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @CommandDefinition(name = "test", description = "testing")
+    public static class TestConsoleCommand implements Command {
+
+        @Option
+        private String bar;
+
+        @Override
+        public CommandResult execute(CommandInvocation commandInvocation) {
+            commandInvocation.print("bar is: "+bar);
+
+            return CommandResult.SUCCESS;
+        }
+    }
+}
