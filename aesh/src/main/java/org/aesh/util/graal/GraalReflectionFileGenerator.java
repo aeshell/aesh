fca0370c82913acc37a0cc97122ed7c3c9406d0b
Ståle Pedersen <stalep@gmail.com>
2019-02-23 09:23:07 +0100

    graal reflection file generation

--- aesh/src/main/java/org/aesh/util/graal/GraalReflectionFileGenerator.java
+++ aesh/src/main/java/org/aesh/util/graal/GraalReflectionFileGenerator.java
@@ -1,18 +1,105 @@
+/*
+ * Copyright 2019 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.aesh.util.graal;
 
 import org.aesh.command.Command;
+import org.aesh.command.impl.internal.ProcessedCommand;
 import org.aesh.command.impl.parser.CommandLineParser;
 import org.aesh.command.invocation.CommandInvocation;
+
+import static org.aesh.utils.Config.getLineSeparator;
 
 /**
  * @author <a href="mailto:stalep@gmail.com">Ståle Pedersen</a>
  */
 public class GraalReflectionFileGenerator {
 
+    private boolean booleanOption = false;
+    private boolean fileOption = false;
+    private boolean hasOptions = false;
+
     public String generateReflection(CommandLineParser<Command<CommandInvocation>> parser) {
+        StringBuilder builder = new StringBuilder("[").append(getLineSeparator());
+        parseCommand(parser.getProcessedCommand(), builder);
+        if(parser.isGroupCommand())
+            for(CommandLineParser<Command<CommandInvocation>> child : parser.getAllChildParsers()) {
+                builder.append("  },").append(getLineSeparator());
+                parseCommand(child.getProcessedCommand(), builder);
+            }
+        if(hasOptions)
+            appendOptions(parser.getProcessedCommand(), builder);
+        else
+            builder.append(getLineSeparator()).append("  }");
 
-        StringBuilder builder = new StringBuilder();
+        return builder.append(getLineSeparator()).append("]").toString();
+    }
 
-        return null;
+    private void parseCommand(ProcessedCommand<Command<CommandInvocation>> command, StringBuilder builder) {
+        builder.append("  {").append(getLineSeparator());
+        appendCommand(command, builder);
+    }
+
+    private void appendOptions(ProcessedCommand<Command<CommandInvocation>> command, StringBuilder builder) {
+        builder.append("  },").append(getLineSeparator());
+        builder.append("  {").append(getLineSeparator())
+                .append("    \"name\" : \"org.aesh.command.impl.parser.AeshOptionParser\", ").append(getLineSeparator());
+        appendDefaults(builder);
+        builder.append(getLineSeparator()).append("  }");
+        if(booleanOption) {
+            builder.append(',').append(getLineSeparator());
+            builder.append("  {").append(getLineSeparator())
+                    .append("    \"name\" : \"org.aesh.command.impl.completer.BooleanOptionCompleter\", ").append(getLineSeparator());
+            appendDefaults(builder);
+            builder.append(getLineSeparator()).append("  }");
+        }
+        if(fileOption) {
+            builder.append(',').append(getLineSeparator());
+            builder.append("  {").append(getLineSeparator())
+                    .append("    \"name\" : \"org.aesh.command.impl.completer.FileOptionCompleter\", ").append(getLineSeparator());
+            appendDefaults(builder);
+            builder.append(getLineSeparator()).append("  }");
+        }
+    }
+
+    private void appendDefaults(StringBuilder builder) {
+        builder.append("    \"allDeclaredConstructors\" : true,").append(getLineSeparator())
+                .append("    \"allPublicConstructors\" : true,").append(getLineSeparator())
+                .append("    \"allDeclaredMethods\" : true,").append(getLineSeparator())
+                .append("    \"allPublicMethods\" : true");
+    }
+
+    private void appendCommand(ProcessedCommand<Command<CommandInvocation>> command, StringBuilder builder) {
+        builder.append("    \"name\" : ").append(command.getCommand().getClass().toString()).append("\",").append(getLineSeparator());
+        appendDefaults(builder);
+
+        if(command.getOptions().size() > 0) {
+            hasOptions = true;
+            builder.append(",").append(getLineSeparator())
+                    .append("    \"fields\" : [").append(getLineSeparator());
+            for(int i=0; i < command.getOptions().size(); i++) {
+                builder.append("      { \"name\" : \"").append(command.getOptions().get(i).getFieldName()).append("\" }");
+                if(i < command.getOptions().size())
+                    builder.append(",").append(getLineSeparator());
+                if(command.getOptions().get(i).type().getName().equalsIgnoreCase("boolean"))
+                    booleanOption = true;
+                if(command.getOptions().get(i).type().getName().equalsIgnoreCase("java.io.file")||
+                    command.getOptions().get(i).type().getName().equalsIgnoreCase("org.aesh.io.resource"))
+                    fileOption = true;
+            }
+            builder.append("    ]").append(getLineSeparator());
+        }
     }
 }
---
+++ aesh/src/test/java/org/aesh/util/graal/GraalReflectionFileGeneratorTest.java
@@ -0,0 +1,131 @@
+package org.aesh.util.graal;
+
+import org.aesh.command.Command;
+import org.aesh.command.CommandDefinition;
+import org.aesh.command.CommandException;
+import org.aesh.command.CommandResult;
+import org.aesh.command.container.CommandContainer;
+import org.aesh.command.container.CommandContainerBuilder;
+import org.aesh.command.impl.container.AeshCommandContainerBuilder;
+import org.aesh.command.impl.parser.CommandLineParser;
+import org.aesh.command.invocation.CommandInvocation;
+import org.aesh.command.option.Option;
+import org.aesh.command.parser.CommandLineParserException;
+import org.aesh.io.Resource;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.aesh.utils.Config.getLineSeparator;
+
+/**
+ * @author <a href="mailto:stalep@gmail.com">Ståle Pedersen</a>
+ */
+public class GraalReflectionFileGeneratorTest {
+
+    @Test
+    public void testSimpleCommand() throws IOException {
+
+        GraalReflectionFileGenerator generator = new GraalReflectionFileGenerator();
+        CommandLineParser parser = getParser(TestCommand1.class);
+
+        String out = generator.generateReflection(parser);
+        assertEquals(readFile("src/test/resources/graal1"), out);
+    }
+
+    @Test
+    public void testCommand() throws IOException {
+
+        GraalReflectionFileGenerator generator = new GraalReflectionFileGenerator();
+        CommandLineParser parser = getParser(TestCommand2.class);
+
+        String out = generator.generateReflection(parser);
+        assertEquals(readFile("src/test/resources/graal2"), out);
+    }
+
+    @Test
+    public void testCommandWithFileOption() throws IOException {
+
+        GraalReflectionFileGenerator generator = new GraalReflectionFileGenerator();
+        CommandLineParser parser = getParser(TestCommand3.class);
+
+        String out = generator.generateReflection(parser);
+        assertEquals(readFile("src/test/resources/graal3"), out);
+    }
+
+
+    private String readFile(String path) throws IOException {
+        StringBuilder builder = new StringBuilder();
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        br.lines().forEach(line -> {
+            if(builder.length() > 0)
+                builder.append(getLineSeparator());
+            builder.append(line);
+        });
+        return builder.toString();
+    }
+
+    private CommandLineParser getParser(Class clazz) {
+        //Class<Command<CommandInvocation>> clazz = loadCommand(command);
+        if (clazz != null) {
+            CommandContainerBuilder<Command<CommandInvocation>, CommandInvocation> builder = new AeshCommandContainerBuilder<>();
+            try {
+                CommandContainer container = builder.create(clazz);
+                return container.getParser();
+            } catch (CommandLineParserException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @CommandDefinition(name = "test1", description = "")
+    public static class TestCommand1 implements Command {
+
+        @Override
+        public CommandResult execute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
+            return CommandResult.SUCCESS;
+        }
+    }
+
+    @CommandDefinition(name = "test2", description = "")
+    public static class TestCommand2 implements Command {
+
+        @Option(shortName = 'h', hasValue = false)
+        private boolean help;
+
+        @Option(shortName = 'o')
+        private boolean override;
+
+        @Option(shortName = 't', defaultValue = {"FOO","BAR"})
+        private String test;
+
+        @Override
+        public CommandResult execute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
+            return CommandResult.SUCCESS;
+        }
+    }
+
+    @CommandDefinition(name = "test3", description = "")
+    public static class TestCommand3 implements Command {
+
+        @Option(shortName = 'h', hasValue = false)
+        private boolean help;
+
+        @Option(shortName = 'o')
+        private boolean override;
+
+        @Option(shortName = 't')
+        private Resource test;
+
+        @Override
+        public CommandResult execute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
+            return CommandResult.SUCCESS;
+        }
+    }
+
+
+}
