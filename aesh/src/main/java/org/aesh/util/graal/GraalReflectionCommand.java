2118b33964f1f8328350fff610dad7a940cf57ae
Tristan Tarrant <ttarrant@redhat.com>
2020-08-31 09:25:23 +0200

    Fix native generator shortcomings:
    * Handle sub command groups
    * Handle custom completers, activators and converters

--- aesh/src/main/java/org/aesh/util/graal/GraalReflectionCommand.java
+++ aesh/src/main/java/org/aesh/util/graal/GraalReflectionCommand.java
@@ -27,6 +27,7 @@ import org.aesh.command.option.Argument;
 import org.aesh.command.option.Option;
 import org.aesh.command.parser.CommandLineParserException;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -36,7 +37,7 @@ import java.nio.file.StandardOpenOption;
  * @author <a href="mailto:stalep@gmail.com">Ståle Pedersen</a>
  */
 @CommandDefinition(name = "graalreflection", description = "Generates a json file to help graal generate a native image")
-public class GraalReflectionCommand implements Command  {
+public class GraalReflectionCommand implements Command {
 
     @Option(hasValue = false)
     private boolean help;
@@ -47,29 +48,25 @@ public class GraalReflectionCommand implements Command  {
 
     @Override
     public CommandResult execute(CommandInvocation commandInvocation) throws CommandException, InterruptedException {
-        if(help) {
+        if (help) {
             commandInvocation.getHelpInfo("graalreflection");
-        }
-        else {
+        } else {
             Class<Command<CommandInvocation>> clazz = loadCommand(command);
-            if(clazz != null) {
+            if (clazz != null) {
                 CommandContainerBuilder<CommandInvocation> builder = new AeshCommandContainerBuilder<>();
                 try {
                     CommandContainer<CommandInvocation> container = builder.create(clazz);
-
                     GraalReflectionFileGenerator graalFileGenerator = new GraalReflectionFileGenerator();
-                    Files.write(Paths.get(container.getParser().getProcessedCommand().name().toLowerCase()+"_reflection.json"),
-                            graalFileGenerator.generateReflection(container.getParser()).getBytes(), StandardOpenOption.CREATE);
-
+                    try (BufferedWriter w = Files.newBufferedWriter(Paths.get(container.getParser().getProcessedCommand().name().toLowerCase() + "_reflection.json"), StandardOpenOption.CREATE)) {
+                        graalFileGenerator.generateReflection(container.getParser(), w);
+                    }
                     container.getParser().getProcessedCommand();
 
-                }
-                catch(CommandLineParserException | IOException e) {
+                } catch (CommandLineParserException | IOException e) {
                     e.printStackTrace();
                 }
-            }
-            else
-                commandInvocation.println("Could not load command: "+command);
+            } else
+                commandInvocation.println("Could not load command: " + command);
         }
         return CommandResult.SUCCESS;
     }
@@ -78,8 +75,7 @@ public class GraalReflectionCommand implements Command  {
     private Class<Command<CommandInvocation>> loadCommand(String commandName) {
         try {
             return (Class<Command<CommandInvocation>>) Class.forName(commandName);
-        }
-        catch(ClassNotFoundException | ClassCastException e) {
+        } catch (ClassNotFoundException | ClassCastException e) {
             e.printStackTrace();
         }
 
